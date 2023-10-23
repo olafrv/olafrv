4d53e9737d73c44d91d2d4b79a2362fd3d131194
Olaf Reitmaier <olafrv@gmail.com>
2023-10-24 00:03:12 +0200

    Foot Mobile

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -17,23 +17,36 @@ span#a33 {
 <template>
   <v-footer>
     <v-container>
-      <v-row class="d-sm-flex">
-        <v-col class="d-sm-flex align-center justify-center">
+      <v-row>
+        <v-col class="text-body-2 text-center align-center">
           Made with:&nbsp;
           <a href="https://vitejs.dev/" target="_blank">
-            <v-img width="25px" src="/img/vite.svg" /> </a
-          >&nbsp;
+            <v-img inline width="25px" src="/img/vite.svg"
+              :aspect-ratio="1"
+            ></v-img>
+          </a>&nbsp;
           <a href="https://vuejs.org/" target="_blank">
-            <v-img width="25px" src="/img/vue.svg" /> </a
-          >&nbsp;
+            <v-img inline width="25px" src="/img/vue.svg"
+              :aspect-ratio="1"
+            ></v-img>
+          </a>&nbsp;
           <a href="https://vuetifyjs.com/" target="_blank">
-            <v-img width="25px" cover src="/img/vuetify.svg" /> </a
-          >&nbsp;
+            <v-img inline width="25px" 
+            :aspect-ratio="1" src="/img/vuetify.svg"
+            ></v-img>
+          </a>&nbsp;
           <a href="https://spline.design/" target="_blank">
-            <v-img width="25px" cover src="/img/spline.png" /> </a
-          >&nbsp; {{ year > 2023 ? "@2023 - " : " @" }} {{ year }}
+            <v-img inline width="25px" 
+              :aspect-ratio="1" src="/img/spline.png"
+            ></v-img>
+          </a>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-body-2 text-center align-center">
+          {{ year > 2023 ? "@2023 - " : " @" }} {{ year }}
           &nbsp;&nbsp;&nbsp;
-          <span class="d-sm-flex-inline" id="a33">Olaf Reitmaier</span>
+          <span id="a33" style="font-size: x-large;">Olaf Reitmaier</span>
         </v-col>
       </v-row>
     </v-container>
