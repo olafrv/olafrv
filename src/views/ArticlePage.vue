7feeb23d9e651f1ebc07a391587f59912f37197f
Olaf Reitmaier <olafrv@gmail.com>
2023-11-16 01:45:40 +0100

    Fix logic

--- src/views/ArticlePage.vue
+++ src/views/ArticlePage.vue
@@ -1,21 +1,24 @@
 <script setup>
-import { ref , onMounted } from 'vue'
+import { ref , onMounted, onBeforeMount } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios';
 
 const route = useRoute()
 const id = ref(route.query.id)
-const error = ref(true)
+const error = ref(false)
+const visible = ref(false)
 const article = ref(null)
 
-onMounted(()=>{
+onBeforeMount(() => {
     const file = '/blog/articles.json'
     axios.get(file).then(function (response){
         article.value = response.data.find((item) => item.id + "" === id.value)
         if (article.value != null) {
-            console.log(article.value)
-            error.value = false
+            visible.value = true
+        }else{
+            error.value = true
         }
+        console.log(article.value)
     })
 })
 </script>
@@ -38,10 +41,10 @@ onMounted(()=>{
                 />
                 Article '{{ id }}' moved or not found, please check the list <router-link to="/articles">here</router-link>.
             </v-row>
-            <v-row v-if="!error" class="text-center">
+            <v-row v-if="visible" class="text-center">
                 <h1>{{ article.title }}</h1>
             </v-row>
-            <v-row v-if="!error" class="text-center">
+            <v-row v-if="visible" class="text-center">
                 <div class="text-center">
                     <iframe :src="article.embed_url" :width="article.width" :height="article.height" frameborder="0" 
                         marginwidth="0" marginheight="0" scrolling="no" allowfullscreen></iframe>
@@ -49,7 +52,7 @@ onMounted(()=>{
                     <a :href="article.url" target="_blank">Download PDF</a>
                 </div>
             </v-row>
-            <v-row v-if="!error" class="text-center">
+            <v-row v-if="visible" class="text-center">
                 <span style="font-size: 10px;">
                     <b>DISCLAIMER:</b> All the content of this website is informative and non-commercial, 
                     does not imply a commitment to develop, launch or schedule delivery 
