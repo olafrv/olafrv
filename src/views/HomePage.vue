e388334f0c7af795dcc20c3c839cb17f84a3f620
Olaf Reitmaier <olafrv@gmail.com>
2025-02-25 20:34:10 +0100

    Warning about WebGL

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,30 +2,64 @@
 import { Application } from '@splinetool/runtime';
 import { ref , onMounted } from 'vue'
 import LoadSpinner from '../components/LoadSpinner.vue'
-// import { useRoute, useRouter } from 'vue-router'
 
 const loading = ref(true);
-const canvas = ref(null)
-// const route = useRoute();
-// const router = useRouter();
+const showImg = ref(false);
+const canvas = ref(null);
+const image = ref(null);
+const image_src = ref("/img/hw_accel_fix.jpeg");
+const url = 'https://prod.spline.design/6KOeVzZFzpWkosQo/scene.splinecode';
 
 onMounted(()=>{
-    // Github Pages trick for calling ArticlesPage.vue
-    // if (route.query.article) {
-    //    router.push({ path: '/article', query: { name: route.query.name } });
-    //    return;
-    // }else{
-        const app = new Application(canvas.value);
-        const url = 'https://prod.spline.design/6KOeVzZFzpWkosQo/scene.splinecode?t=' + Math.random();
+    const app = new Application(canvas.value);
+    let webgl = false;
+    for (const context of ['webgl2', 'webgl', 'experimental-webgl', 'moz-webgl', 'webkit-3d']) {
+        try {
+            canvas.value.getContext(context);
+            webgl = true;
+            break;
+        } catch (e) {
+            continue;
+        }
+    }
+    webgl = false;
+    if (!webgl) {
+        console.log("WebGL not supported");
+        showImg.value = true;
+        loading.value = false;
+        
+    }else{
+        console.log("WebGL supported");
         app.load(url).then(()=>{
             loading.value = false;
         });
-    // }
+    }
 })
 </script>
 <template>
     <v-container class="text-center d-flex flex-column">
         <LoadSpinner v-show="loading"/>
-        <canvas ref="canvas" v-show="`!loading`"></canvas>
+        <div class="text-left" v-if="showImg">
+            <v-alert
+                icon="mdi-alert"
+                color="info"
+                title="Browser without WebGL support"
+                text=""
+            >
+            <br>
+            The content of this section requires WebGL support to be displayed.
+            <br>
+            Go to Settings → System → Enable Hardware Acceleration.
+            <br>
+            <br>
+            <v-img ref="image"
+                :height="`400`"
+                :src="`${image_src}`" aspect-ratio="16/9" v-show="`!showImg`"/>
+            
+            </v-alert>
+        </div>
+        <div v-if="!showImg">
+            <canvas ref="canvas" v-show="`!loading`"></canvas>
+        </div>
     </v-container>
 </template>
