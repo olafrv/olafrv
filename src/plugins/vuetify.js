35d449026f35a98acec7e230c4fee82abd70c2ba
Olaf Reitmaier <olafrv@gmail.com>
2025-04-14 02:18:57 +0200

    Add missing Roboto Font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,15 +2,13 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/features/icon-fonts/#mdi-css
-// import "@mdi/font/css/materialdesignicons.css"; // development
-// import "@mdi/font/css/materialdesignicons.min.css"; // development (minimized)
+// import "@mdi/font/css/materialdesignicons.css"; // full
+// mport "@mdi/font/css/materialdesignicons.min.css"; // minimized
 // import { aliases, mdi } from "vuetify/iconsets/mdi"; // development
-
 // https://vuetifyjs.com/en/features/icon-fonts/#mdi-js-svg
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'; // production (optimal)
 
 import { md2 } from "vuetify/blueprints";
-import { mdiAccount } from "@mdi/js";
 
 export default createVuetify({
   theme: {
